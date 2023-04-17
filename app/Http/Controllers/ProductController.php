8ec6008c07fc04651191a07273489ee435e99ef9
Jowam <69232719+dioame@users.noreply.github.com>
2023-04-17 21:13:48 +0800

    Initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $getAllProduct = Product::all();
+        return $getAllProduct ? ProductResource::collection($getAllProduct) : response()->json(['status'=>404]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(){}
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $name = $request->json('name');
+        $product = new Product();
+        $product->name = $name;
+        return $product->save() ? response()->json(['status'=>200]) :  response()->json(['status'=>500]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $getProduct = Product::find($id);
+        return $getProduct ? new ProductResource($getProduct) : response()->json(['status'=>404]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id){}
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $name = $request->json('name');
+        $product = Product::find($id);
+        $product->name = $name;
+        return $product->save() ? response()->json(['status'=>200]) :  response()->json(['status'=>500]);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        return Product::find($id)->delete() ? response()->json(['status'=>200]) : response()->json(['status'=>404]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::resource('/api/products', ProductController::class)->only('index','show','store','update','destroy');
+
+Route::get('/', function () {
+    return view('welcome');
+});
